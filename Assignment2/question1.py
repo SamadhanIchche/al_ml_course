01c25aa4bcbadf99bcbbbdb7352540cec4ae049f
Sam <samadhanpatilichche009@gmail.com>
2026-06-16 23:28:21 +0530

    question 4 completed

--- Assignment2/question1.py
+++ Assignment2/question1.py
@@ -1,2 +1,9 @@
-while Trugit :
-    print("Hello Guys")
+num = int(input("Enter a Number here :"))
+counter = 1
+while (counter <= 10):
+    if (counter % 2 == 0):
+        counter +=1
+        continue
+        
+    print(f"{counter} * {num} = {counter*num}")
+    counter +=1 
